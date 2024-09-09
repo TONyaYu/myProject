a08baf90428394dae7aac5fe2f9e7c816f6dd9fd
Tonya Yurina <tonyabatonya@yandex.ru>
2024-09-09 18:42:30 +0300

    create multimodule project

---
+++ service/src/main/java/AppRun.java
@@ -0,0 +1,8 @@
+import org.example.service.UserService;
+
+public class AppRun {
+    public static void main(String[] args) {
+        var userService = new UserService();
+        System.out.println(userService.getUser(1L));
+    }
+}
