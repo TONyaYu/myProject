d974da146cf800c709c23e03cc1d4629f80a33fb
Tonya Yurina <tonyabatonya@yandex.ru>
2024-09-10 16:48:12 +0300

    add some corrections

---
+++ common/src/test/java/org/example/entity/UserTest.java
@@ -0,0 +1,13 @@
+package org.example.entity;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserTest {
+    @Test
+    public void UserDaoTest() {
+        assertTrue(true);
+    }
+
+}
