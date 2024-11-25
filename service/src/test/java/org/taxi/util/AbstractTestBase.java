7c3938ce6104f9f4408676adf3ea086f058fa375
Tonya Yurina <tonyabatonya@yandex.ru>
2024-11-25 19:16:54 +0300

    fixes

--- service/src/test/java/org/taxi/util/AbstractTestBase.java
+++ service/src/test/java/org/taxi/util/AbstractTestBase.java
@@ -1,15 +1,12 @@
 package org.taxi.util;
 
 import org.hibernate.Session;
-import org.hibernate.SessionFactory;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.taxi.config.AppConfig;
-
-import java.lang.reflect.Proxy;
 
 public abstract class AbstractTestBase {
 
@@ -19,16 +16,16 @@ public abstract class AbstractTestBase {
     @BeforeAll
     static void createApplicationContext() {
         applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
-        }
+    }
 
     @BeforeEach
-    void openSession(){
+    void beginTransaction() {
         session = applicationContext.getBean(Session.class);
         session.getTransaction().begin();
     }
 
     @AfterEach
-    void closeSession(){
+    void closeTransaction() {
         session.getTransaction().rollback();
     }
 
