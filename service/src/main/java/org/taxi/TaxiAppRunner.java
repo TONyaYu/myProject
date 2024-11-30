d017505a567bdfe27bf85747e6ad28f8fd271dd0
Tonya Yurina <tonyabatonya@yandex.ru>
2024-11-30 16:55:08 +0300

    update config

--- service/src/main/java/org/taxi/TaxiAppRunner.java
+++ service/src/main/java/org/taxi/TaxiAppRunner.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TaxiAppRunner {
 
     public static void main(String[] args) {
-        SpringApplication.run(TaxiAppRunner.class);
+        SpringApplication.run(TaxiAppRunner.class, args);
     }
 }
 
