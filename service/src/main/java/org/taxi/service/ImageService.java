2ece4117e23954e26a06ae2e3ff867e5c28f99bf
Tonya Yurina <tonyabatonya@yandex.ru>
2024-12-16 17:54:07 +0300

    SpringREST format

--- service/src/main/java/org/taxi/service/ImageService.java
+++ service/src/main/java/org/taxi/service/ImageService.java
@@ -2,13 +2,11 @@ package org.taxi.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.StandardOpenOption;
 import java.util.Optional;
 
 import static java.nio.file.StandardOpenOption.CREATE;
@@ -24,7 +22,7 @@ public class ImageService {
     public void uploadImage(String imagePath, InputStream content) {
         Path fullImagePath = Path.of(bucket, imagePath);
 
-        try(content){
+        try (content) {
             Files.createDirectories(fullImagePath.getParent());
             Files.write(fullImagePath, content.readAllBytes(), CREATE, TRUNCATE_EXISTING);
         }
